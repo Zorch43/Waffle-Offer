f17d034e9852b70625d40b8927d8a12f174bac63
Brady Hyde <bradyhyde10@gmail.com>
2016-04-06 11:20:59 -0700

    Merge branch 'Trade' into Items
    # Conflicts:
    #	WaffleOffer/WaffleOffer/Models/Item.cs

---
+++ WaffleOffer/Models/WaffleOfferDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WaffleOffer.Models
+{
+    public class WaffleOfferDBContext : IdentityDbContext<AppUser>
+    {
+        public WaffleOfferDBContext()
+            : base("name=WaffleOfferDBContext")
+        {
+        }
+
+        //public System.Data.Entity.DbSet<WaffleOffer.Models.Trader> Traders { get; set; }
+    }
+}

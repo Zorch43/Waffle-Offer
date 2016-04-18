9e009727cf06b3ea5760feaa556d600aa174e224
Timothy Cooper <Cooper.T.0043@gmail.com>
2016-04-17 20:48:20 -0700

    Trade view created
    added a link to the item details view that allows the user to open a
    trade with the owner of the item.

--- WaffleOffer/WaffleOffer/Models/Trade.cs
+++ WaffleOffer/WaffleOffer/Models/Trade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace WaffleOffer.Models
 {
     public class Trade
     {
+
+        //key
+        [Key]
+        public int TradeId { get; set; }
+
+        //sending trader
+        public AppUser SendingTrader { get; set; }
+        public List<Item> SendingItems { get; set; }
+
+        //recieving trader
+        public AppUser ReceivingTrader { get; set; }
+        public List<Item> ReceivingItems { get; set; }
+
     }
 }

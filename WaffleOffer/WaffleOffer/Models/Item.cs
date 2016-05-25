6080624b42a96194789c0eada13d086a23699ecd
Brady Hyde <bradyhyde10@gmail.com>
2016-05-25 11:18:37 -0700

    TODO Comments

--- WaffleOffer/WaffleOffer/Models/Item.cs
+++ WaffleOffer/WaffleOffer/Models/Item.cs
@@ -16,19 +16,19 @@ namespace WaffleOffer.Models
         [Key]
         public int ItemID { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } // replace with title
         [Required]
-        public string Description { get; set; }
-        public List<ItemTag> Tags { get { return tags; } set { tags = value; } }
+        public string Description { get; set; } //  description from api
+        public List<ItemTag> Tags { get { return tags; } set { tags = value; } } // if they come from api, maybe change to categories
         public List<ItemImage> Images { get { return images; } set { images = value; } }
         [Required]
         public int Quality { get; set; }
         //  What the Item is measured in (minutes, pounds, etc)
         [Required]
-        public string Units { get; set; }
+        public string Units { get; set; } // don't need
         //  number of Units offered
         [Required]
-        public double Quantity { get; set; }
+        public double Quantity { get; set; } // don't need
         //type of listing
         public ItemType ListingType { get; set; }
         //listing user
